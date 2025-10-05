3dc297f1657b5eee729d03182ceba1e9e4be7aaa
abhis <abhishekchauhan4871@gmail.com>
2025-10-05 22:43:17 +0530

    search screen created

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,15 +1,92 @@
-import { Text, View } from "react-native";
+import MovieCard from "@/components/MovieCard";
+import SearchBar from "@/components/SearchBar";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
+import { useEffect, useState } from "react";
+import { ActivityIndicator, FlatList, Image, Text, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function Search() {
   const insets = useSafeAreaInsets();
+  const [searchQuery, setSearchQuery] = useState('')
+
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+    refetch: loadMovies,
+    reset
+  } = useFetch(() =>
+    fetchMovies({
+      query: searchQuery,
+    }), false)
+
+    useEffect(() => {
+      const func = async () => {
+        if(searchQuery.trim()) {
+        await loadMovies();
+      } else {
+        reset()
+      }
+      }
+
+      func()
+    }, [searchQuery])
 
   return (
     <View 
-      style= {{paddingTop: insets.top}}
-      className="flex-1  bg-slate-800 "
+      style={{ paddingTop: insets.top, paddingBottom: insets.bottom }}
+      className="flex-1  bg-primary"
     >
-      <Text className="font-bold text-4xl text-white">Movie Mood</Text>
+      <Image source={images.bg} className="flex-1 absolute w-full z-0" resizeMode="cover"/>
+      <FlatList
+      data={movies}
+      renderItem={({item}) => <MovieCard {...item}/>}
+      keyExtractor={(item) => item.id.toString()}
+      className="px-5"
+      numColumns={3}
+      columnWrapperStyle={{
+        justifyContent: "flex-start",
+        gap: 12,
+        marginVertical: 16
+      }}
+      contentContainerStyle={{paddingBottom: 100}}
+      ListHeaderComponent={
+        <>
+          <View className="w-full flex-row justify-center mt-10  items-center">
+            <Image source={icons.logo} className="w-12 h-10"/>
+          </View>
+          <View className="my-5">
+            <SearchBar 
+              placeholder="Search movies..."
+              value={searchQuery}
+              onChangeText={(text: string) => setSearchQuery(text)}
+            />
+          </View>
+
+          {moviesLoading && (
+            <ActivityIndicator
+                size="large"
+                color={"#FF6467"}
+                className="mt-10 self-center"
+              />
+          )}
+          {moviesError && (
+            <Text className="text-red-500 px-5 my-3">
+              Error: {moviesError.message}
+            </Text>
+          )}
+          {!moviesLoading && !moviesError && searchQuery.trim() && movies?.length > 0 && (
+            <Text className="text-xl text-white font-bold">
+              Search results for{' '}
+              <Text className="text-ui_400">{searchQuery}</Text>
+            </Text>
+          )}
+        </>
+      }
+      />
     </View>
   );
 }
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -3,11 +3,13 @@ import React from "react";
 import Ionicons from "@expo/vector-icons/Ionicons";
 
 interface Props {
-  placeholder: string,
+  placeholder: string;
+  value?: string,
   onPress?: () => void;
+  onChangeText?: (text: string) => void;
 }
 
-const SearchBar = ({placeholder, onPress}: Props) => {
+const SearchBar = ({placeholder, onPress, value, onChangeText }: Props) => {
 
   return (
     <View className="relative z-10 rounded-full">
@@ -19,41 +21,12 @@ const SearchBar = ({placeholder, onPress}: Props) => {
           placeholder={placeholder}
           placeholderTextColor={"white"}
           cursorColor={"white"}
-          // onChangeText={handleTextDebounce}
+          value={value}
+          onChangeText={onChangeText}
           className="p-2 h-15 pb-2 flex-1 text-xl rounded-lg ml-2 text-white"
         />
         <Ionicons name="search" size={20} color="black" className="mr-2" />
-        {/* <TouchableOpacity
-          onPress={() => setShowSearch(!showSearch)}
-          activeOpacity={1}
-          className="rounded-full p-3 m-1"
-          style={{backgroundColor: theme.bgWhite(0.3)}}
-        >
-          <Ionicons name="search" size={20} color="black" className="mr-2" />
-        </TouchableOpacity> */}
       </View>
-      {/* {
-          locations.length > 0 && showSearch?(
-            <View className="absloute w-full bg-gray-300 top-3 rounded-3xl">
-              {
-                locations.map((loc, index) => {
-                  let showBorder = index+1 != locations.length;
-                  let bordeClass = showBorder? 'border-b-2 border-b-gray-400' : '';
-                  return(
-                    <TouchableOpacity
-                      onPress={() => handleLocation(loc)}
-                      key={index}
-                      className={"flex-row items-center border-0 p-3 px-4 mb-1 " + bordeClass}
-                    >
-                      <MapPinIcon size={20} color={'gray'}/>
-                      <Text className="text-black text-lg ml-2">{loc?.name}, {loc?.country}</Text>
-                    </TouchableOpacity>
-                  )
-                })
-              }
-            </View>
-          ): null
-        } */}
     </View>
   );
 };
